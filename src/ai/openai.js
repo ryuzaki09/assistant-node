25babd673dee0ba5ff71d011e2894b1c9b5c6aac
Sin Luong <sin@finervision.com>
2025-03-03 22:02:35 +0000

    added prettier.

--- src/ai/openai.js
+++ src/ai/openai.js
@@ -1,41 +1,41 @@
-import { OpenAI as OpenAILib } from "openai";
-import "dotenv/config";
+import {OpenAI as OpenAILib} from 'openai'
+import 'dotenv/config'
 
-const MAX_HISTORY = 10;
+const MAX_HISTORY = 10
 
 export class OpenAI {
-  openAI = null;
-  chatHistory = [];
+  openAI = null
+  chatHistory = []
 
   constructor(apiKey) {
     if (!this.openAI) {
-      this.openAI = new OpenAILib({ apiKey: process.env.OPENAI_KEY });
+      this.openAI = new OpenAILib({apiKey: process.env.OPENAI_KEY})
     }
   }
 
   async createChat(text) {
-    this.chatHistory.push({ role: "user", content: text });
+    this.chatHistory.push({role: 'user', content: text})
 
     if (this.chatHistory.length > MAX_HISTORY) {
-      this.chatHistory.shift();
+      this.chatHistory.shift()
     }
-    console.log("chathistory: ", this.chatHistory);
+    console.log('chathistory: ', this.chatHistory)
     const response = await this.openAI.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: this.chatHistory,
-    });
+      model: 'gpt-3.5-turbo',
+      messages: this.chatHistory
+    })
 
-    const aiResponse = response.choices[0].message.content;
-    this.chatHistory.push({ role: "assistant", content: aiResponse });
+    const aiResponse = response.choices[0].message.content
+    this.chatHistory.push({role: 'assistant', content: aiResponse})
 
-    return aiResponse;
+    return aiResponse
   }
 
   async textToSpeech(text) {
     return this.openAI.audio.speech.create({
-      model: "tts-1",
+      model: 'tts-1',
       input: text,
-      voice: "alloy",
-    });
+      voice: 'alloy'
+    })
   }
 }
--- src/services/mqtt.js
+++ src/services/mqtt.js
@@ -1,17 +1,17 @@
 import mqtt from 'mqtt'
 
-const mqttClient = mqtt.connect(
-  `mqtt://${process.env.MQTT_HOST}:1883`, 
-  {username: process.env.MQTT_USERNAME, password: process.env.MQTT_PASSWORD}
-);
+const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_HOST}:1883`, {
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD
+})
 
 export const mqttService = {
   sendToHomeAssistant: (text) => {
-    mqttClient.publish("homeassistant/voice/incoming", text)
+    mqttClient.publish('homeassistant/voice/incoming', text)
   }
 }
 
-mqttClient.subscribe("homeassistant/voice/outgoing")
+mqttClient.subscribe('homeassistant/voice/outgoing')
 
 mqttClient.on('message', (topic, message) => {
   if (topic === 'homeassistant/voice/outgoing') {
